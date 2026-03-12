e0b5be1b9d422f2cba76d8c0a2a5c9025b7431f9
viniciusofp <viniciusofp@gmail.com>
2026-03-12 15:09:21 -0300

    common deploy on netlify

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  basePath: '/observatorio-vpg'
-};
-
-export default nextConfig;
